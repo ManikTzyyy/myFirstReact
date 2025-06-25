4e08dc4fa4bda3221a5dcd5091f50cc889756d69
ManikTzyyy <manikyogantara1@gmail.com>
2025-06-25 22:51:03 +0800

    creating case for atomic design concept

--- src/components/elements/button/index.jsx
+++ src/components/elements/button/index.jsx
@@ -1,15 +1,27 @@
 import { Children } from "react";
 
 const Button = (props) => {
-  const {children ="..."} = props
+  const { children = "..." } = props;
   return (
     <button
       type="button"
-      className="rounded px-6 pb-2 pt-2.5 text-xs font-medium text-white w-full bg-blue-700"
+      className="rounded px-6 py-2 text-xs font-medium text-white w-full bg-blue-700"
     >
       {children}
     </button>
   );
 };
 
-export default Button
+function ButtonWhite(props) {
+  const { children = "..." } = props;
+  return (
+    <button
+      type="button"
+      className="rounded px-6 py-2 text-xs font-medium text-blue-700 w-full bg-white"
+    >
+      {children}
+    </button>
+  );
+}
+
+export {Button, ButtonWhite};
