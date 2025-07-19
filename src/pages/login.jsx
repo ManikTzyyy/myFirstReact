4c145c41f6be39c8611d813efceb49b5f8919fde
ManikTzyyy <manikyogantara1@gmail.com>
2025-07-19 15:39:32 +0800

    implementasi redux menggunakan toolkit reducer

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,11 +1,16 @@
 import AuthLayout from "../components/layout/AuthLayout";
 import FormLogin from "../components/fragments/FormLogin";
 
-
 export default function LoginPage() {
   return (
-    <AuthLayout title="Login" type="login">
-      <FormLogin />
-    </AuthLayout>
+    <>
+      <ul className="flex justify-center items-center gap-5">
+        <li>username : johnd</li>
+        <li>password : m38rmF$</li>
+      </ul>
+      <AuthLayout title="Login" type="login">
+        <FormLogin />
+      </AuthLayout>
+    </>
   );
 }
